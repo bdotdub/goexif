84d7bd8af151df9da46fd164759f52f91f902226
Benny Wong <benny@bwong.net>
2014-01-13 19:05:20 -0500

    rwcarlsen -> bdotdub

--- mknote/mknote.go
+++ mknote/mknote.go
@@ -4,8 +4,8 @@ package mknote
 import (
 	"bytes"
 
-	"github.com/rwcarlsen/goexif/exif"
-	"github.com/rwcarlsen/goexif/tiff"
+	"github.com/bdotdub/goexif/exif"
+	"github.com/bdotdub/goexif/tiff"
 )
 
 var (
